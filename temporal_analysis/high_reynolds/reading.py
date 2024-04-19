7a6020727181160c30a969c74e18a96082b68dd3
Luca <lucagontijo911@outlook.com>
2024-04-19 11:42:40 -0300

    added plot and table

--- temporal_analysis/high_reynolds/reading.py
+++ temporal_analysis/high_reynolds/reading.py
@@ -2,6 +2,9 @@ import pandas as pd
 import sys
 sys.path.append("..")
 from statistics_for_turbulence import ExperimentalData
+import numpy as np
+from scipy.stats import linregress
+import pandas as pd
 
 # Define the file path
 file_path = 'data_from_experiment/re_temp/hre/hre1.dat'
@@ -12,8 +15,54 @@ data = pd.read_csv(file_path, delimiter='\s+', header=None)
 # Extract the columns into separate variables
 time = data[0].tolist()
 speed_inst = data[1].tolist()
+typical_dimension = 0.05
 
 # Create an instance of the ExperimentalData class
-hre1 = ExperimentalData("HRE1", time, speed_inst, 0.1)
+hre1 = ExperimentalData("HRE1", time, speed_inst, typical_dimension)
 hre1.process()
 
+# Print the results as a table
+import matplotlib.pyplot as plt
+
+results = {
+    "Mean Average": hre1.mean_average,
+    "Variance": hre1.variance,
+    "Standard Deviation": hre1.standard_deviation,
+    "Turbulent Kinetic Energy": hre1.turbulent_kinetic_energy,
+    "Turbulent Intensity": hre1.turbulent_intensity,
+    "Reynolds Number": hre1.reynolds_number,
+    "Coefficient of Kurtosis": hre1.coefficient_of_kurtosis,
+    "Coefficient of Skewness": hre1.coefficient_of_skewness
+}
+
+df = pd.DataFrame(results.items(), columns=["Parameter", "Value"])
+
+# Plot the table as an image
+fig, ax = plt.subplots(figsize=(8, 4))
+ax.axis('off')
+ax.axis('tight')
+ax.table(cellText=df.values, colLabels=df.columns, loc='center')
+plt.title('Results')
+plt.savefig('results_table.png')
+plt.show()
+
+import matplotlib.pyplot as plt
+
+# Create a scatter plot of speed vs density probability
+plt.scatter(hre1.raw_speed, hre1.density_of_probability)
+plt.xlabel('Speed')
+plt.ylabel('Density Probability')
+plt.title('Density Probability vs Speed')
+
+# Calculate the Gaussian density of probability
+mu = hre1.mean_average
+sigma = hre1.standard_deviation
+x = np.linspace(min(hre1.raw_speed), max(hre1.raw_speed), 100)
+y = (1 / (sigma * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x - mu) / sigma) ** 2)
+
+# Plot the Gaussian density of probability
+plt.plot(x, y, color='red', label='Density of Probability')
+
+plt.legend()
+
+plt.show()
