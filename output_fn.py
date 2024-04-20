731c016bf5eb2ff65424c6c1767b8b29af82f8f9
Luca <lucagontijo911@outlook.com>
2024-04-20 11:39:49 -0300

    refactored code

---
+++ output_fn.py
@@ -0,0 +1,54 @@
+import os
+import pandas as pd
+import sys
+import numpy as np
+from statistics_for_turbulence import ExperimentalData
+from scipy.stats import linregress
+import matplotlib.pyplot as plt
+
+def plot_table(results, filename):
+    """
+    Plot the results as a table and save the image.
+
+    Args:
+    - results (dict): Dictionary containing results to be plotted.
+    - filename (str): Name of the file for saving the plot.
+    """
+    df = pd.DataFrame(results.items(), columns=["Parameter", "Value"])
+    fig, ax = plt.subplots(figsize=(8, 4))
+    ax.axis('off')
+    ax.axis('tight')
+    ax.table(cellText=df.values, colLabels=df.columns, loc='center')
+    plt.title(f'Experiment Results - {filename}')
+    plt.savefig(f'images/tables/results_table_{filename}.png')
+
+def plot_density_probability(experimental_data, filename):
+    """
+    Plot Gaussian density of probability and save the image.
+
+    Args:
+    - experimental_data (ExperimentalData): ExperimentalData object.
+    - filename (str): Name of the file for saving the plot.
+    """
+    mu = experimental_data.mean_average
+    sigma = experimental_data.standard_deviation
+    x = np.linspace(min(experimental_data.raw_speed), max(experimental_data.raw_speed), 100)
+    y = (1 / (sigma * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x - mu) / sigma) ** 2)
+    plt.plot(x, y, color='red', label='Density of Probability')
+    plt.title(f'Experiment Results - {filename} - Density of Probability')
+    plt.legend()
+    plt.savefig(f'images/plots/{filename}_density_probability_plot.png')
+
+def plot_raw_speed_time(experimental_data, filename):
+    """
+    Plot raw_time vs raw_speed and save the image.
+
+    Args:
+    - experimental_data (ExperimentalData): ExperimentalData object.
+    - filename (str): Name of the file for saving the plot.
+    """
+    plt.plot(experimental_data.raw_time, experimental_data.raw_speed)
+    plt.xlabel('Time')
+    plt.ylabel('Speed')
+    plt.title(f'Experiment Results - {filename} - Raw Speed vs Raw Time')
+    plt.savefig(f'images/plots/{filename}_raw_speed_time_plot.png', dpi=300)
--- reading_fn.py
+++ reading_fn.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import sys
 import numpy as np
+from output_fn import plot_density_probability, plot_raw_speed_time, plot_table
 from statistics_for_turbulence import ExperimentalData
 from scipy.stats import linregress
 import matplotlib.pyplot as plt
@@ -31,73 +32,3 @@ def read_data(folder_path):
     
     return processed_data
 
-def plot_table(results, filename):
-    """
-    Plot the results as a table and save the image.
-
-    Args:
-    - results (dict): Dictionary containing results to be plotted.
-    - filename (str): Name of the file for saving the plot.
-    """
-    df = pd.DataFrame(results.items(), columns=["Parameter", "Value"])
-    fig, ax = plt.subplots(figsize=(8, 4))
-    ax.axis('off')
-    ax.axis('tight')
-    ax.table(cellText=df.values, colLabels=df.columns, loc='center')
-    plt.title(f'Experiment Results - {filename}')
-    plt.savefig(f'images/tables/results_table_{filename}.png')
-
-def plot_density_probability(experimental_data, filename):
-    """
-    Plot Gaussian density of probability and save the image.
-
-    Args:
-    - experimental_data (ExperimentalData): ExperimentalData object.
-    - filename (str): Name of the file for saving the plot.
-    """
-    mu = experimental_data.mean_average
-    sigma = experimental_data.standard_deviation
-    x = np.linspace(min(experimental_data.raw_speed), max(experimental_data.raw_speed), 100)
-    y = (1 / (sigma * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x - mu) / sigma) ** 2)
-    plt.plot(x, y, color='red', label='Density of Probability')
-    plt.title(f'Experiment Results - {filename} - Density of Probability')
-    plt.legend()
-    plt.savefig(f'images/plots/{filename}_density_probability_plot.png')
-
-def plot_raw_speed_time(experimental_data, filename):
-    """
-    Plot raw_time vs raw_speed and save the image.
-
-    Args:
-    - experimental_data (ExperimentalData): ExperimentalData object.
-    - filename (str): Name of the file for saving the plot.
-    """
-    plt.plot(experimental_data.raw_time, experimental_data.raw_speed)
-    plt.xlabel('Time')
-    plt.ylabel('Speed')
-    plt.title(f'Experiment Results - {filename} - Raw Speed vs Raw Time')
-    plt.savefig(f'images/plots/{filename}_raw_speed_time_plot.png', dpi=300)
-
-def main(folder_path):
-    processed_data = read_data(folder_path)
-
-    for file, experimental_data in processed_data.items():
-        results = {
-            "Mean Average": round(experimental_data.mean_average, 2),
-            "Variance": round(experimental_data.variance, 2),
-            "Standard Deviation": round(experimental_data.standard_deviation, 2),
-            "Turbulent Kinetic Energy": round(experimental_data.turbulent_kinetic_energy, 2),
-            "Turbulent Intensity": round(experimental_data.turbulent_intensity, 2),
-            "Reynolds Number": round(experimental_data.reynolds_number, 2),
-            "Coefficient of Kurtosis": round(experimental_data.coefficient_of_kurtosis, 2),
-            "Coefficient of Skewness": round(experimental_data.coefficient_of_skewness, 2)
-        }
-        plot_table(results, file)
-        plot_density_probability(experimental_data, file)
-        plot_raw_speed_time(experimental_data, file)
-
-if __name__ == "__main__":
-    # Call the main function for each program
-    folder_paths = ['data_from_experiment/re_temp']
-    for folder_path in folder_paths:
-        main(folder_path)
