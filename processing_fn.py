9adff6f4ff631177abb487a64059756d346a896d
Luca <lucagontijo911@outlook.com>
2024-04-20 14:38:26 -0300

    added code to process statistical data

--- processing_fn.py
+++ processing_fn.py
@@ -1,8 +1,8 @@
 import os
 import pandas as pd
-from statistics_for_turbulence import ExperimentalData
+from statistics_for_turbulence import ExperimentalData, mean
 
-def process_data(folder_path, dat_files):
+def temporal(folder_path, dat_files):
     processed_data = {}
     
     for file in dat_files:
@@ -17,3 +17,21 @@ def process_data(folder_path, dat_files):
     
     return processed_data
 
+def probabilistic(processed_data):
+    first_experimental_data = list(processed_data.values())[0]
+    raw_speed = []
+    raw_time = first_experimental_data.raw_time
+    description = 'Probabilistic'
+    typical_dimension = 0.05
+    experimental_data_prob = ExperimentalData(description, raw_time, raw_speed, typical_dimension)
+    instant_speed = []
+
+    for file, experimental_data in processed_data.items():
+        instant_speed.append(experimental_data.raw_speed)
+    
+    grouped_elements = zip(*instant_speed)
+    statistical_speed = [mean(group) for group in grouped_elements]
+    experimental_data_prob.raw_speed = statistical_speed
+    
+    experimental_data_prob.process()
+    return experimental_data_prob
