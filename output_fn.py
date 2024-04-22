fe8008490170d0f97a3ead49199a02efb97cef05
Luca <lucagontijo911@outlook.com>
2024-04-22 15:09:36 -0300

    run main and generated figs

--- output_fn.py
+++ output_fn.py
@@ -35,9 +35,14 @@ def plot_density_probability(experimental_data, filename):
     sigma = experimental_data.standard_deviation
     x = np.linspace(min(experimental_data.raw_speed), max(experimental_data.raw_speed), 100)
     y = (1 / (sigma * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x - mu) / sigma) ** 2)
-    plt.plot(x, y, color='red', label='Gaussian Density of Probability')
+    plt.plot(x, y, color='red', label='Densidade de Probabilidade')
+    sigma_normal = 1
+    y_normal = (1 / (sigma_normal * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x - mu) / sigma_normal) ** 2)
+    plt.plot(x, y_normal, color = 'green', label = 'Densidade Gaussiana Normal')
     plt.hist(experimental_data.raw_speed, density=True, bins=20, alpha=0.5, label='Experimental Density of Probability')
-    plt.title(f'Experiment Results - {filename} - Density of Probability')
+    plt.xlabel('Velocidade (m/s)')
+    plt.ylabel('Contagem')
+    plt.title(f'Resultados Experimentais - {filename} - Densidade de Probabilidade')
     plt.legend()
     plt.savefig(f'images/plots/{filename}_density_probability_plot.png')
     plt.close()
@@ -51,9 +56,9 @@ def plot_raw_speed_time(experimental_data, filename):
     - filename (str): Name of the file for saving the plot.
     """
     plt.plot(experimental_data.raw_time, experimental_data.raw_speed)
-    plt.xlabel('Time')
-    plt.ylabel('Speed')
-    plt.title(f'Experiment Results - {filename} - Raw Speed vs Raw Time')
+    plt.xlabel('Tempo (s)')
+    plt.ylabel('Velocidade (m/s)')
+    plt.title(f'EResultados Experimentais - {filename} - Tempo vs Velocidade')
     plt.savefig(f'images/plots/{filename}_raw_speed_time_plot.png', dpi=300)
     plt.close()
 
@@ -68,9 +73,9 @@ def plot_profile_speed(speed, z_positions, description):
     """
     plt.plot(speed, z_positions)
     plt.scatter(speed, z_positions)
-    plt.xlabel('Speed')
-    plt.ylabel('Z Positions')
-    plt.title(f'Experiment Results - {description} - Speed Profile')
+    plt.xlabel('Velocidade (m/s)')
+    plt.ylabel('Altura z (cm)')
+    plt.title(f'Resultados Experimentais - {description} - Perfil de Velocidade')
     plt.savefig(f'images/plots/{description}_speed_profile_plot.png')
     plt.close()
 
@@ -85,8 +90,8 @@ def plot_profile_turbulent_intensity(turbulent_intensity, z_positions, descripti
     """
     plt.scatter(turbulent_intensity, z_positions)
     plt.plot(turbulent_intensity, z_positions)
-    plt.xlabel('Turbulent Intensity')
-    plt.ylabel('Z Positions')
-    plt.title(f'Experiment Results - {description} - Turbulent Intensity Profile')
+    plt.xlabel('Intensidade Turbulenta (%)')
+    plt.ylabel('Altura em z (cm)')
+    plt.title(f'Resultados Experimentais - {description} - Perfil de Intensidade Turbulenta')
     plt.savefig(f'images/plots/{description}_turbulent_intensity_profile_plot.png')
     plt.close()
